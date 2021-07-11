e7fac19da8041c5574a1d35fe180ba93ddf67d32
lguerrero1 <luisguerrero12387@gmail.com>
2021-07-10 21:40:24 -0400

    adding citas, nueva citas and pacientes
    working with axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Pacientes from './components/Pacientes';
 import NuevaCita from './components/nuevaCita';
 import Cita from './components/cita';
+
+import clienteAxios from './config/axios';
+
 function App() {
-  console.log()
+
+  const [citas, guardarCitas] = useState([]);
+  const [consulta, guardarConsulta] = useState(true);
+
+  useEffect(() => {
+    
+        if (consulta) {
+          const consultarAPI = () => {
+            clienteAxios.get('/pacientes')
+            .then(res =>{
+              //colocar en el state el resultado
+              guardarCitas(res.data);
+
+              //deshabilitar la consulta
+              guardarConsulta(false);
+            })
+            .catch(error => {
+              console.log(error);
+            })
+          }
+
+          consultarAPI();
+      }
+  }, [consulta]);
 
   return (
     <Router>
       <Switch>
         <Route
           exact path ="/"
-          component={Pacientes}
+          component={() => <Pacientes citas={citas}  />}
         />
          <Route
           exact path ="/nueva"
-          component={NuevaCita}
+          component={() => <NuevaCita  guardarConsulta={guardarConsulta} />}
         />
          <Route
-          exact path ="/cita:id"
-          component={Cita}
+          exact path ="/cita/:id"
+          render={(props) => {
+            const cita = citas.filter( cita => cita._id === props.match.params.id);
+            console.log('linea 50',cita)
+            return(
+              <Cita
+                cita={cita}
+              />
+            )
+          }}        
         />
   
       </Switch>
--- src/components/cita.js
+++ src/components/cita.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
-const Cita = () => {
-    return ( <h1>desde Cita</h1> );
+const Cita = (props) => {
+    if (!props.cita) {
+        props.history.push('/');
+        return null;
+    }
+    const {cita: {nombre,propietario, fecha, hora, telefono, sintomas } } = props;
+    return ( 
+        <Fragment>
+            <h1 className="my-5">Nombre cita: {nombre}</h1>
+            <div className="container mt-5 py-5">
+                <div className="row">
+                    <div className="col-12 mb-5 d-flex justify-content-center">
+                        <Link to={'/'} className="btn btn-success text-uppercase py-2 px-5 font-weight-bold">volver</Link>                    
+                    </div>
+                </div>
+                <div className="col-md-8 mx-auto">
+                    <div className="list-group">
+                        <div className="p-5 list-group-item list-group-item-action flex-column alig-items-center">
+                            
+                        <div className="d-flex w-100 justify-content-between mb-4">
+                                        <h3 className="mb-3">{nombre}</h3>
+                                        <small className="fecha-alta">
+                                            {fecha} - {hora}
+                                        </small>
+                                    </div>
+
+                                    <p className="mb-0">
+                                        {sintomas}
+                                    </p>
+                                    <div className="contacto py-3">
+                                        <p>Dueño: {propietario}</p>
+                                        <p>Teléfono: {telefono}</p>
+                                    </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+        );
 }
  
-export default Cita;
+export default withRouter(Cita);
